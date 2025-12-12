d248e7ae6c33416660280618f397995742f752a0
Shadow <das840kunal@gmail.com>
2025-12-12 10:15:22 +0530

    chore: initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
+import prettier from "eslint-config-prettier/flat";
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+	...nextVitals,
+	// Override default ignores of eslint-config-next.
+	globalIgnores([
+		// Default ignores of eslint-config-next:
+		".next/**",
+		"out/**",
+		"build/**",
+		"next-env.d.ts",
+	]),
+	{
+		rules: {
+			"no-console": ["warn", { allow: ["warn", "error"] }],
+			"no-unused-vars": [
+				"warn",
+				{ argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+			],
+			"prefer-const": "warn",
+		},
+	},
+	prettier,
 ]);
 
 export default eslintConfig;
